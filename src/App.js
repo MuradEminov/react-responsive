a1b2c7cc50d31347a58d4e9259d0f42257019d9c
MuradEminov <murademinov88@gmail.com>
2020-08-20 14:08:25 +0400

    Jumbotron ready and added to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import Navigation from './components/Navigation';
+import MyJumbotron from './components/Jumbotron';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Form inline id="search">
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-      </Navbar>
-      
-
+    <div style={{textAlign:"center"}}>
+      <Navigation />
+      <div className="container">
+      <MyJumbotron />
+      </div>
       <h1>React Bootstrap</h1>
 
     </div>
